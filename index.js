cb89ea194c1108e0a0b9942d3c3cc91c4fd4582c
Kimber Herron <kh582013@gmail.com>
2022-09-21 13:51:14 -0400

    commit tree 627fc58cddf9043bb219ee9bf29b5cec1c1af136

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function (plan = 'go to the office') {
+    return `This Monday, I will ${plan}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function inner(parameter = 'special!'){
+    return `You are ${flair}${parameter}${flair}!`
+    }
+}
+//wrapAdjective('you are')('parameter');
+    //const outer = 'you are'
+    //const inner = function (idk = 'a dedicated programmer') {
+    //return `${outer}, ${idk}` }
+
+//wrapAdjective('*'')('a dedicated programmer');
